0ef815b84e0f0d1ca24b003aadbc6c7fde3ae83f
Thoma55s <thomas.morgan.sager@gmail.com>
2018-12-02 11:59:23 -0500

    Added Governments and more names

--- townMaker.py
+++ townMaker.py
@@ -7,7 +7,7 @@ def getName(x): #gets a name from a txt file
 			nameArray.append(line)
 	
 	randomName = random.randint(0,len(nameArray) - 1)
-	return nameArray[randomName][:-1] # deletes enter character
+	return nameArray[randomName][:-1] # deletes enter character must have blank line at end of txt files
 
 def getTownName(): #gets a prefix and suffix to make a town name
 	return getName("townPrefix.txt") + getName("townSuffix.txt")
@@ -16,9 +16,12 @@ def getPopulation():
 	range = random.randint(0,300)
 	return 20 + range
 
+
+
 		
 print("Name: " + getTownName())
 print("Worships: " + getName("dieties.txt"))
 print("Primarily Populated with: " + getName("races.txt"))
 print("Town Leader is a: " + getName("classes.txt"))
+print("Government: " + getName("governments.txt"))
 print("Population: " + str(getPopulation()))
